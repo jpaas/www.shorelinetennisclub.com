21ee0fbeaa0aad9af4810290ed8db9121190b881
alfong9 <124890347+alfong9@users.noreply.github.com>
2024-02-26 17:46:30 -0500

    Updated Mon & Wed Court booking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,12 +104,10 @@ export default function Home() {
             </Link>
           </p>
           <p>
-            Courts 1 and 2 are reserved on Mondays, for a round robin from 7 to 9 pm for players of a 3.0 level or
-            higher.
+          Court 1 and 2 are booked for Doubles Round Robins on Mondays and Wednesdays from 7 to 9 pm.
           </p>
           <p>
-            Courts 1 and 2 are reserved on Wednesdays, for a round robin from 7 to 9 pm for players of 2.5 level or
-            higher.
+          Mondays are mainly for men, and Wednesdays are mainly for women. However, all genders are welcome to join.
           </p>
           <p className="pt-2">
             One court might be used for Little Aces tennis from 4:30 to 6:30 pm on weekdays. Junior lessons from ages 6
