3097b338eca67e385d2703591768ef35312544a4
alfong9 <124890347+alfong9@users.noreply.github.com>
2026-03-05 16:48:00 -0500

    Update page.tsx
    Round Robin updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,9 +103,9 @@ export default function Home() {
               Court Booking Rules
             </Link>
           </p>
-          <p>Court 1 and 2 are booked for Doubles Round Robins on Mondays and Wednesdays from 7 to 9 pm.</p>
+          <p>Court 1 and 2 are booked for Doubles Round Robins on Mondays from 7 to 9 pm.</p>
           <p>
-            Mondays are mainly for men, and Wednesdays are mainly for women. However, all genders are welcome to join.
+            Mondays are mainly for men; however, all genders are welcome to join.
           </p>
           {/*<p className="pt-2">
             Junior lessons from ages 6 to teens are provided by{" "}
